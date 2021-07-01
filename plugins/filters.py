48f1d776c78f3b722ebbca9a8a5d98454b83f552
sunaif-adkar <86029415+sunaif-adkar@users.noreply.github.com>
2021-07-01 19:19:49 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -184,7 +184,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("BACK", callback_data="start_data"),
                     InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ SUPPORT ⭕️", url="https://t.me/")]
+                [InlineKeyboardButton("⭕️ SUPPORT ⭕️", url="https://t.me/sabotssupport")]
             ])
 
             await query.message.edit_text(
